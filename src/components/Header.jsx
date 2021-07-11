ed509bf7300216b1fa8cc6f056406cff3170a634
Igor Pechenkin <ipechenkin@gmail.com>
2021-07-11 23:19:17 +0300

    basic components of app

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.vhv.rs/dpng/d/573-5732595_logo-maker-shop-make-your-logo-in-5.png" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.markgray.com.au/images/gallery/large/desert-light.jpg" />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
