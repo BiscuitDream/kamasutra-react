d5b2a17516d283a7fde1f156687ec5ae30c3ab4a
Igor Pechenkin <ipechenkin@gmail.com>
2021-07-19 21:11:23 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Switch, Route, Redirect } from "react-router-dom";
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 const App = () => {
@@ -11,8 +15,14 @@ const App = () => {
       <Header />
       <Navbar />
       <div className="app-wrapper-content">
-        {/*<Profile />*/}
-        <Dialogs />
+        <Switch>
+          <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
+          <Redirect from="/" to="/profile" />
+        </Switch>
       </div>
     </div>
   );
