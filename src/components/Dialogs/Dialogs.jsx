5fa75c0f099e0854870824ca471575fd74475e00
Igor Pechenkin <ipechenkin@gmail.com>
2021-07-20 14:44:22 +0300

    add dialogs and fix lists

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,34 @@
-import classes from './Dialogs.module.css'
+import styles from './Dialogs.module.css'
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
-    </div>
+    <section className={styles.dialogsBlock}>
+      <ul className={styles.dialogs}>
+        <li className={styles.dialog + ' ' + styles.active}>
+          Dimych
+        </li>
+        <li className={styles.dialog}>
+          Andrey
+        </li>
+        <li className={styles.dialog}>
+          Sveta
+        </li>
+        <li className={styles.dialog}>
+          Sasha
+        </li>
+        <li className={styles.dialog}>
+          Viktor
+        </li>
+        <li className={styles.dialog}>
+          Valera
+        </li>
+      </ul>
+      <ul className={styles.messages}>
+        <li className={styles.message}>Hi</li>
+        <li className={styles.message}>How is your Yo?</li>
+        <li className={styles.message}>Yo!</li>
+      </ul>
+    </section>
   );
 };
 
