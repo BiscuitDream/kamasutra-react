1ef8177448aedcc707d897649a51b66b6ebcabfd
Igor Pechenkin <ipechenkin@gmail.com>
2021-07-25 00:48:10 +0300

    fix move datas to index.js

--- src/App.js
+++ src/App.js
@@ -8,37 +8,15 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 
-const postsData = [
-  { id: 1, message: 'Hi, how are you?', likesCount: 15 },
-  { id: 2, message: 'It\'s my first post', likesCount: 20 },
-];
-
-const dialogsData = [
-  { id: 1, name: 'Dimych' },
-  { id: 2, name: 'Andrey' },
-  { id: 3, name: 'Sveta' },
-  { id: 4, name: 'Sasha' },
-  { id: 5, name: 'Viktor' },
-  { id: 6, name: 'Valera' },
-];
-
-const messagesData = [
-  { id: 1, text: 'Hi'},
-  { id: 2, text: 'How is your Yo?'},
-  { id: 3, text: 'Yo!'},
-  { id: 4, text: 'Yo!'},
-  { id: 5, text: 'Yo!'},
-];
-
-const App = () => {
+const App = (props) => {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
       <div className="app-wrapper-content">
         <Switch>
-          <Route path="/profile" render={() => <Profile postsData={postsData} />} />
-          <Route path="/dialogs" render={() => <Dialogs dialogsData={dialogsData} messagesData={messagesData} />} />
+          <Route path="/profile" render={() => <Profile postsData={props.postsData} />} />
+          <Route path="/dialogs" render={() => <Dialogs dialogsData={props.dialogsData} messagesData={props.messagesData} />} />
           <Route path="/news" component={News} />
           <Route path="/music" component={Music} />
           <Route path="/settings" component={Settings} />
--- src/index.js
+++ src/index.js
@@ -5,10 +5,32 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+const postsData = [
+  { id: 1, message: 'Hi, how are you?', likesCount: 15 },
+  { id: 2, message: 'It\'s my first post', likesCount: 20 },
+];
+
+const dialogsData = [
+  { id: 1, name: 'Dimych' },
+  { id: 2, name: 'Andrey' },
+  { id: 3, name: 'Sveta' },
+  { id: 4, name: 'Sasha' },
+  { id: 5, name: 'Viktor' },
+  { id: 6, name: 'Valera' },
+];
+
+const messagesData = [
+  { id: 1, text: 'Hi'},
+  { id: 2, text: 'How is your Yo?'},
+  { id: 3, text: 'Yo!'},
+  { id: 4, text: 'Yo!'},
+  { id: 5, text: 'Yo!'},
+];
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App postsData={postsData} dialogsData={dialogsData} messagesData={messagesData} />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
