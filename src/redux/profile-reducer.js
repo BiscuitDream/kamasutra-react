d2961b40eee172137e8fe07ff74d897ca36e0cc9
Igor Pechenkin <ipechenkin@gmail.com>
2021-08-23 02:12:07 +0300

    refactor profile reducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -14,23 +14,24 @@ const initialState = {
 
 const profileReducer = (state = initialState, action) => {
   switch (action.type) {
-    case UPDATE_NEW_POST_TEXT: {
-      const stateCopy = {...state};
-      stateCopy.newPostText = action.newText;
-      return stateCopy;
-      }
-    case ADD_POST: { // TODO наверно лучше сразу копировать весь стейт
+    case UPDATE_NEW_POST_TEXT:
+      return  {
+        ...state,
+        newPostText: action.newText
+      };
+
+    case ADD_POST: // TODO наверно лучше сразу копировать весь стейт
       const newPost = {
         id: 5,
         message: state.newPostText,
         likesCount: 0,
       };
-      const stateCopy = {...state};
-      stateCopy.posts = [...state.posts];
-      stateCopy.posts.push(newPost);
-      stateCopy.newPostText = '';
-      return stateCopy;
-    }
+      return {
+        ...state,
+        posts: [...state.posts, newPost],
+        newPostText: ''
+      };
+
     default:
       return state;
   }
