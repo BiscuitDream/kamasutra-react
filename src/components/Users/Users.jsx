c58b18a5290b646687b2c02c39dbf8576b1966c0
Igor Pechenkin <ipechenkin@gmail.com>
2021-09-10 00:15:55 +0300

    Users. presentaional in container in container component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,70 +1,49 @@
 import React from 'react';
 import styles from './Users.module.css';
-import axios from "axios";
 import userPhoto from "../../assets/images/user.png";
 
-class Users extends React.Component {
-  componentDidMount() {
-    if (this.props.users.length === 0) {
-      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-        .then(response => {
-          this.props.setUsers(response.data.items);
-          this.props.setTotalUsersCount(response.data.totalCount);
-        });
-    }
+const Users = (props) => { // TODO пользователей заменить на список и убрать бредовую разметку. сейчас блочные элементы в строковые вложены
+  const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+  const pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    const page = <li className={(props.currentPage === i) && styles.selectedPage}
+                     onClick={() => props.onPageChanged(i)}>{i}</li>; // TODO по идее надо обработчик вешать не на все страницы, а на список просто
+    pages.push(page);
   }
 
-  onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-      .then(response => {
-        this.props.setUsers(response.data.items);
-      });
-  }
-
-  render() { // TODO пользователей заменить на список и убрать бредовую разметку. сейчас блочные элементы в строковые вложены
-    const pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-    const pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-      const page = <li className={(this.props.currentPage === i) && styles.selectedPage}
-                       onClick={() => this.onPageChanged(i)}>{i}</li>;
-      pages.push(page);
-    }
-
-    return ( // TODO переделать пагинацию. там сейчас список из 100500 страниц
+  return ( // TODO переделать пагинацию. там сейчас список из 100500 страниц
+    <div>
       <div>
-        <div>
-          <ul className={styles.paginationList}>
-            {pages}
-          </ul>
-        </div>
-        {this.props.users.map((user) => (
-        <div key={user.id}>
-          <span>
-            <div>
-              <img className={styles.userPhoto} src={user.photos.small || userPhoto} width="100px" />
-            </div>
-            <div>
-              {user.followed
-                ? <button onClick={() => {this.props.unfollow(user.id)}}>Unfollow</button>
-                : <button onClick={() => {this.props.follow(user.id)}}>Follow</button>}
-            </div>
-          </span>
-          <span>
-            <span>
-              <div>{user.name}</div>
-              <div>{user.status}</div>
-            </span>
-            <span>
-              <div>{"user.location.country"}</div>
-              <div>{"user.location.city"}</div>
-            </span>
-          </span>
-        </div>))}
+        <ul className={styles.paginationList}>
+          {pages}
+        </ul>
       </div>
-    );
-  }
-}
+      {props.users.map((user) => (
+        <div key={user.id}>
+        <span>
+          <div>
+            <img className={styles.userPhoto} src={user.photos.small || userPhoto} width="100px" />
+          </div>
+          <div>
+            {user.followed
+              ? <button onClick={() => {props.unfollow(user.id)}}>Unfollow</button>
+              : <button onClick={() => {props.follow(user.id)}}>Follow</button>}
+          </div>
+        </span>
+          <span>
+          <span>
+            <div>{user.name}</div>
+            <div>{user.status}</div>
+          </span>
+          <span>
+            <div>{"user.location.country"}</div>
+            <div>{"user.location.city"}</div>
+          </span>
+        </span>
+        </div>))}
+    </div>
+  );
+};
 
 export default Users;
