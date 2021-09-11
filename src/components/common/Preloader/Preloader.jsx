1c35424e8f9d9f4095ef0111de0d065af438d1ba
Igor Pechenkin <ipechenkin@gmail.com>
2021-09-11 23:46:02 +0300

    link to example

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -1,6 +1,7 @@
 import styles from './Preloader.module.css';
 import {ReactComponent as PreloaderImage} from './preloader.svg';
 
+// https://codepen.io/nikhil8krishnan/pen/rVoXJa
 const Preloader = () => { // TODO картинку лучше отцентрировать
   return (
     <div className={styles.overlay}>
