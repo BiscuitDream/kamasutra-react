04af24904efc8fad09b63bc7d22276dda48fb1f2
Igor Pechenkin <ipechenkin@gmail.com>
2021-09-12 13:48:20 +0300

    preloader horizontal alignment

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -2,7 +2,7 @@ import styles from './Preloader.module.css';
 import {ReactComponent as PreloaderImage} from './preloader.svg';
 
 // https://codepen.io/nikhil8krishnan/pen/rVoXJa
-const Preloader = () => { // TODO картинку лучше отцентрировать
+const Preloader = () => { // TODO надо бы выключить скролл при лоадере
   return (
     <div className={styles.overlay}>
       <PreloaderImage className={styles.preloader} />
