b84ee68d74328213ac65e75e505353a056f34bde
Igor Pechenkin <ipechenkin@gmail.com>
2021-10-03 19:09:40 +0300

    add techs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// React(UI), Redux(BLL), CSS Modules, axios, react-router-dom(Switch, Route, NavLink, Link, withRouter)
+// React(UI), Redux(BLL), Redux Thunk, CSS Modules, axios, react-router-dom(Switch, Route, NavLink, Link, withRouter, Redirect)
 import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom';
