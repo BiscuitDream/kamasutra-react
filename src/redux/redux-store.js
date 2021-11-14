514f74273c23a90e2cf8594763e40f249de59eb6
Igor Pechenkin <ipechenkin@gmail.com>
2021-11-14 19:56:09 +0300

    add redux devtools extension for browser

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
@@ -16,8 +16,10 @@ const reducers = combineReducers({
   form: formReducer
 });
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+// const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 export default store;
 
-window.store = store; // TODO for debug, delete this
+// window.store = store; // TODO for debug, delete this
